ded99a5c7073c0370f3bce4f12444a80a8fd2911
catpouch <pouchofcats@gmail.com>
2024-12-26 23:34:22 -0800

    graphing(!) + BASIC error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use eframe::egui::{self, text::{CCursor, CCursorRange}};
+use eframe::egui::{self, text::{CCursor, CCursorRange}, FontData, FontDefinitions};
 use egui::Key;
 use crate::parser::CatwareCalc;
+use egui_plot::{Line, Plot, PlotPoints};
 
 pub mod parser;
-// use egui_plot::{Line, Plot, PlotPoints};
 
 fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions::default();
@@ -12,13 +12,23 @@ fn main() -> eframe::Result {
 
 struct CatwareApp {
     input: String,
-    history: Vec<(String, f64)>,
+    history: Vec<(String, Result<f64, Box<dyn std::error::Error>>)>,
     history_index: usize,
     parser: CatwareCalc,
+    plot_shown: bool
 }
 
 impl CatwareApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // let mut fonts = FontDefinitions::default();
+        // fonts.font_data.insert("Fira Code".to_owned(), 
+        // std::sync::Arc::new(
+        //     // .ttf and .otf supported
+        //     FontData::from_static(include_bytes!("../../../epaint_default_fonts/fonts/Ubuntu-Light.ttf"))
+        // ));
+
+        // cc.egui_ctx.set_fonts(font_definitions);
+
         cc.egui_ctx.style_mut(|style| {
             style.visuals.extreme_bg_color = style.visuals.faint_bg_color;
         });
@@ -27,75 +37,89 @@ impl CatwareApp {
             input: "glorp".to_owned(),
             history: vec![],
             history_index: usize::MAX,
-            parser: CatwareCalc::new()
+            parser: CatwareCalc::new(),
+            plot_shown: false
         }
     }
 }
 
 impl eframe::App for CatwareApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // egui::SidePanel::right("graph_panel").show(ctx, |ui| {
-        //     Plot::new("main_plot").show(ui, |plot_ui| plot_ui.line(Line::new(PlotPoints::new(self.points.clone())))); // FIX THIS
-        // });
-
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(self.history.clone().into_iter().map(|(cmd, result)| {
-                format!("> {0}\n{1}\n", cmd, result)
-            }).fold(String::new(), |a,b| a + &b));
-
-            ui.horizontal(|ui| {
-                ui.label(">");
-                let input_box_widget = egui::TextEdit::singleline(&mut self.input).frame(false);
-                let mut input_box= input_box_widget.show(ui);
-                if input_box.response.changed() && self.history_index != usize::MAX {
-                    self.history[self.history_index].0 = self.input.clone();
-                }
-
-                ctx.input(|i| {
-                    if i.key_pressed(Key::Enter) {
-                        if self.history_index != usize::MAX {
-                            self.history.truncate(self.history_index);
-                        }
-
-                        let result = self.parser.parse_string(&self.input);
-
-                        if result.is_some() {
-                            self.history.push((self.input.clone(), result.unwrap()));
-                        }
-
-                        self.history_index = usize::MAX;
-
-                        self.input.clear();
-                    }
-    
-                    if i.key_pressed(Key::ArrowUp) {
-                        if self.history_index == usize::MAX {
-                            self.history_index = self.history.len();
-                        }
-                        if self.history_index > 0 {
-                            self.history_index -= 1;
-                            self.input = self.history[self.history_index].0.clone();
-                        }
-
-                        input_box.state.cursor.set_char_range(Some(CCursorRange::one(CCursor::new(self.input.len()))));
-                    }
-    
-                    if i.key_pressed(Key::ArrowDown) {
-                        if self.history_index == self.history.len() - 1 {
-                            self.history_index = usize::MAX;
-                            self.input = "".to_owned();
-                        } else if self.history_index != usize::MAX {
-                            self.history_index += 1;
-                            self.input = self.history[self.history_index].0.clone();
-                        }
-
-                        input_box.state.cursor.set_char_range(Some(CCursorRange::one(CCursor::new(self.input.len()))));
-                    }
+            if self.plot_shown {
+                let plot = Plot::new("main_plot");
+                plot.show(ui, |plot_ui| {
+                    plot_ui.line(Line::new(PlotPoints::new(self.parser.plot_points.borrow().to_vec())));
+                    let _ = self.parser.refresh_graph(plot_ui.plot_bounds());
                 });
+            }
 
-                input_box.state.store(ui.ctx(), input_box.response.id);
-                
-                input_box.response.request_focus();
+            egui::ScrollArea::vertical().stick_to_bottom(true).show(ui, |ui| {
+                ui.label(self.history.iter().map(|(cmd, result)| {
+                    format!("> {0}\n{1:?}\n", cmd, result)
+                }).fold(String::new(), |a,b| a + &b));
+    
+                ui.horizontal(|ui| {
+                    ui.label(">");
+                    let input_box_widget = egui::TextEdit::singleline(&mut self.input).frame(false);
+                    let mut input_box= input_box_widget.show(ui);
+                    if input_box.response.changed() && self.history_index != usize::MAX {
+                        self.history[self.history_index].0 = self.input.clone();
+                    }
+    
+                    ctx.input(|i| {
+                        if i.key_pressed(Key::Enter) {
+                            if self.history_index != usize::MAX {
+                                self.history.truncate(self.history_index);
+                            }
+    
+                            // i know this is bad. i'm going insane
+                            if self.input.starts_with("plot") {
+                                self.plot_shown = true;
+                            }
+    
+                            let result = self.parser.eval_string(&self.input);
+
+                            self.history.push((self.input.clone(), result));
+    
+                            self.history_index = usize::MAX;
+    
+                            self.input.clear();
+                        }
+        
+                        if i.key_pressed(Key::ArrowUp) {
+                            if self.history_index == usize::MAX {
+                                self.history_index = self.history.len();
+                            }
+                            if self.history_index > 0 {
+                                self.history_index -= 1;
+                                self.input = self.history[self.history_index].0.clone();
+                            }
+    
+                            input_box.state.cursor.set_char_range(Some(CCursorRange::one(CCursor::new(self.input.len()))));
+                        }
+        
+                        if i.key_pressed(Key::ArrowDown) {
+                            if self.history_index == self.history.len() - 1 {
+                                self.history_index = usize::MAX;
+                                self.input = "".to_owned();
+                            } else if self.history_index != usize::MAX {
+                                self.history_index += 1;
+                                self.input = self.history[self.history_index].0.clone();
+                            }
+    
+                            input_box.state.cursor.set_char_range(Some(CCursorRange::one(CCursor::new(self.input.len()))));
+                        }
+    
+                        if i.key_pressed(Key::F4) {
+                            self.plot_shown = !self.plot_shown;
+                        }
+                    });
+    
+                    input_box.state.store(ui.ctx(), input_box.response.id);
+                    
+                    input_box.response.request_focus();
+                });
             });
         });
     }
--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 use core::f64;
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap};
 
+use egui_plot::PlotBounds;
 use pest::{iterators::{Pair, Pairs}, pratt_parser::{Assoc, Op, PrattParser}, Parser};
 use pest_derive::Parser;
 
@@ -41,7 +42,10 @@ pub struct CatwareCalc {
     vars_context: HashMap<String, f64>, // contains user-defined and hardcoded values
     hardcoded_vars: [&'static str; 3], // list of names of hardcoded values to prevent user from overwriting them
     funcs_context: HashMap<String, CatwareFunc>, // contains user-defined functions
-    hardcoded_funcs: [&'static str; 18] // list of names of hardcoded functions to prevent user from defining an existing function twice
+    hardcoded_funcs: [&'static str; 18], // list of names of hardcoded functions to prevent user from defining an existing function twice
+    plot_resolution: usize,
+    pub plot_points: RefCell<Vec<[f64; 2]>>,
+    plot_func: RefCell<String>,
 }
 
 impl CatwareCalc {
@@ -65,27 +69,42 @@ impl CatwareCalc {
             hardcoded_vars: ["pi", "tau", "e"],
             funcs_context: HashMap::new(),
             // i am aware this is kind of awful
-            hardcoded_funcs: ["ln", "log2", "log10", "sin", "asin", "sinh", "asinh", "cos", "acos", "cosh", "acosh", "tan", "atan", "tanh", "atanh", "sqrt", "cbrt", "abs"]
+            hardcoded_funcs: ["ln", "log2", "log10", "sin", "asin", "sinh", "asinh", "cos", "acos", "cosh", "acosh", "tan", "atan", "tanh", "atanh", "sqrt", "cbrt", "abs"],
+            plot_resolution: 100,
+            plot_points: Vec::new().into(),
+            plot_func: RefCell::new("x".to_owned())
         }
     }
 
-    fn handle_func(&self, primary: Pair<Rule>, context: &HashMap<String, f64>) -> Option<f64> {
+    // evaluates functions (user-defined and built in)
+    fn handle_func(&self, primary: Pair<Rule>, context: &HashMap<String, f64>) -> Result<f64, Box<dyn std::error::Error>> {
         let mut primary_inner = primary.into_inner();
         let first = primary_inner.next().unwrap();
         if first.as_rule() != Rule::id {
-            Some(self.parse_expr(first.into_inner()))
+            self.eval_expr(first.into_inner())
         } else {
-            let args: Vec<f64> = primary_inner.next().unwrap().into_inner().map(|a| self.parse_expr_context(a.into_inner(), &context)).collect();
+            // let args: Vec<f64> = primary_inner.next().unwrap().into_inner().map(|a| self.eval_expr_context(a.into_inner(), &context)).collect();
+            let mut arg_pairs: Pairs<Rule> = primary_inner.next().unwrap().into_inner();
             if self.funcs_context.contains_key(first.as_str()) {
                 let func_def = &self.funcs_context[first.as_str()];
-                if args.len() != func_def.signature.len() {return None}
+                if arg_pairs.len() != func_def.signature.len() {return Err(Box::new(std::fmt::Error))}
+                let args: Vec<f64> = arg_pairs.map(|a| -> Result<f64, Box<dyn std::error::Error>> {Ok(self.eval_expr_context(a.into_inner(), &context)?)}).collect::<Result<_, _>>()?; // may god have mercy on my soul
                 let new_context: HashMap<String, f64> = args.iter().enumerate().map(|a| {(func_def.signature[a.0].clone(), a.1.to_owned())}).collect::<HashMap<_, _, _>>();
-                let parsed_pairs=  CatwareParser::parse(Rule::program, &func_def.definition).expect("parse failed").next().unwrap().into_inner().next().unwrap().into_inner();
-                Some(self.parse_expr_context(parsed_pairs, &new_context))
+                let parsed_pairs=  CatwareParser::parse(Rule::program, &func_def.definition)?.next().unwrap().into_inner().next().unwrap().into_inner();
+                self.eval_expr_context(parsed_pairs, &new_context)
             } else {
-                match args.len() {
+                // TODO: this is not good
+                match arg_pairs.len() {
                     1 => {
                         let output: f64;
+                        if first.as_str() == "plot" {
+                            println!("{:#?}", arg_pairs);
+                            let mut plot_func_ref = self.plot_func.borrow_mut();
+                            *plot_func_ref = arg_pairs.as_str().to_owned();
+                            self.plot_func(&mut arg_pairs, [-10.0, 10.0]);
+                            return Err(Box::new(std::fmt::Error))
+                        }
+                        let args: Vec<f64> = arg_pairs.map(|a| -> Result<f64, Box<dyn std::error::Error>> {Ok(self.eval_expr_context(a.into_inner(), &context)?)}).collect::<Result<_, _>>()?;
                         match first.as_str() {
                             "ln" => output = args[0].ln(),
                             "log2" => output = args[0].log2(),
@@ -105,71 +124,106 @@ impl CatwareCalc {
                             "sqrt" => output = args[0].sqrt(),
                             "cbrt" => output = args[0].cbrt(),
                             "abs" => output = args[0].abs(),
-                            _ => return None
+                            _ => return Err(Box::new(std::fmt::Error))
                         }
-                        return Some(output)
-                    }
-                    _ => None
+                        Ok(output)
+                    },
+                    // 2 => {
+                        
+                    // }
+                    _ => Err(Box::new(std::fmt::Error))
                 }
             }
         }
     }
 
-    fn handle_assignment(&mut self, pairs: Pairs<Rule>) {
+    fn plot_func(&self, arg_pairs: &mut Pairs<Rule>, bounds: [f64; 2]) -> Result<(), Box<dyn std::error::Error>> {
+        let expression = arg_pairs.next().unwrap();
+        // let x_var_pair = arg_pairs.next().unwrap().into_inner().next().unwrap();
+        // if x_var_pair.as_rule() != Rule::id {return}
+        // let x_var = x_var_pair.as_str().to_owned();
+        let mut points = self.plot_points.borrow_mut();
+        points.clear();
+        for i in 0..self.plot_resolution {
+            let x_val = ( i as f64 / self.plot_resolution as f64 ) * (bounds[1] - bounds[0]) + bounds[0];
+            let context = HashMap::from([
+                ("x".to_owned(), x_val)
+            ]);
+            let point_y = self.eval_expr_context(expression.clone().into_inner(), &context)?;
+            points.push([x_val, point_y]);
+        }
+        Ok(())
+    }
+
+    // handles assigning values to vars & functions
+    fn handle_assignment(&mut self, pairs: Pairs<Rule>) -> Result<(), Box<dyn std::error::Error>> {
         let mut mut_pairs = pairs.clone();
         let lhs = mut_pairs.next().unwrap();
         if lhs.as_rule() == Rule::id {
             let val = mut_pairs.skip(1).next().unwrap().into_inner();
-            if self.hardcoded_vars.contains(&lhs.as_str()) {return}
-            self.vars_context.insert(lhs.as_str().to_owned(), self.parse_expr(val));
+            if self.hardcoded_vars.contains(&lhs.as_str()) {return Err(Box::new(std::fmt::Error))}
+            self.vars_context.insert(lhs.as_str().to_owned(), self.eval_expr(val)?);
         } else {
             let mut id= lhs.into_inner();
             let name = id.next().unwrap().as_str();
-            if self.hardcoded_funcs.contains(&name) {return}
+            if self.hardcoded_funcs.contains(&name) {return Err(Box::new(std::fmt::Error))}
             self.funcs_context.insert(name.to_owned(), CatwareFunc::new(id.next().unwrap().into_inner().map(|a| a.as_str().to_owned()).collect(), mut_pairs.skip(1).next().unwrap().as_str().to_owned()));
         }
+        Ok(())
     }
 
-    fn parse_expr(&self, pairs: Pairs<Rule>) -> f64 {
-        self.parse_expr_context(pairs, &self.vars_context)
+    // wrapper to parse expressions using the CatwareCalc's context
+    fn eval_expr(&self, pairs: Pairs<Rule>) -> Result<f64, Box<dyn std::error::Error>> {
+        self.eval_expr_context(pairs, &self.vars_context)
     }
 
-    fn parse_expr_context(&self, pairs: Pairs<Rule>, context: &HashMap<String, f64>) -> f64 {
+    // handles the variety of operations within a statement
+    fn eval_expr_context(&self, pairs: Pairs<Rule>, context: &HashMap<String, f64>) -> Result<f64, Box<dyn std::error::Error>> {
         self.pratt
             .map_primary(|primary| match primary.as_rule() {
-                Rule::num  => primary.as_str().parse().unwrap(),
-                Rule::expr => self.parse_expr_context(primary.into_inner(), &context),
-                Rule::func => self.handle_func(primary, context).unwrap(),
-                Rule::id   => {context.get(primary.as_str()).unwrap().to_owned()},
+                Rule::num  => Ok(primary.as_str().parse::<f64>()?),
+                Rule::expr => self.eval_expr_context(primary.into_inner(), &context),
+                Rule::func => self.handle_func(primary, context),
+                Rule::id   => Ok(context.get(primary.as_str()).copied().ok_or(Box::new(std::fmt::Error))?), // dear god
                 _          => unreachable!(),
             })
             .map_prefix(|op, rhs| match op.as_rule() {
-                Rule::neg  => -rhs,
+                Rule::neg  => Ok(-rhs?),
                 _          => unreachable!(),
             })
             // .map_postfix(|lhs, op| match op.as_rule() {
             //     Rule::fac  => (1..lhs+1).product(),
             //     _          => unreachable!(),
             // })
-            .map_infix(|lhs, op, rhs| match op.as_rule() {
-                Rule::add  => lhs + rhs,
-                Rule::sub  => lhs - rhs,
-                Rule::mul  => lhs * rhs,
-                Rule::div  => lhs / rhs,
-                Rule::pow  => lhs.powf(rhs),
-                _          => unreachable!(),
+            .map_infix(|lhs, op, rhs| {
+                let lhs_val = lhs?;
+                let rhs_val = rhs?;
+                match op.as_rule() {
+                    Rule::add  => Ok(lhs_val + rhs_val),
+                    Rule::sub  => Ok(lhs_val - rhs_val),
+                    Rule::mul  => Ok(lhs_val * rhs_val),
+                    Rule::div  => Ok(lhs_val / rhs_val),
+                    Rule::pow  => Ok(lhs_val.powf(rhs_val)),
+                    _          => unreachable!(),
+                }
             })
             .parse(pairs)
     }
 
-    pub fn parse_string(&mut self, input: &str) -> Option<f64> {
-        // may god save us all
-        let first_parsed = CatwareParser::parse(Rule::program, input).expect("parse failed").next().unwrap().into_inner().next().unwrap();
+    pub fn refresh_graph(&self, bounds: PlotBounds) -> Result<(), Box<dyn std::error::Error>> {
+        let plot_func = self.plot_func.borrow(); // just realized these names are terrible. i will not be fixing it
+        let mut pairs = CatwareParser::parse(Rule::program, plot_func.as_str())?.next().unwrap().into_inner();
+        self.plot_func(&mut pairs, [bounds.min()[0], bounds.max()[0]])
+        // self.plot_func(&mut CatwareParser::parse(Rule::program, self.plot_func.borrow().as_str()).unwrap(), [bounds.min()[0], bounds.max()[0]]);
+    }
+
+    pub fn eval_string(&mut self, input: &str) -> Result<f64, Box<dyn std::error::Error>> {
+        let first_parsed = CatwareParser::parse(Rule::program, input)?.next().unwrap().into_inner().next().unwrap();
         if first_parsed.as_rule() == Rule::assignment {
             self.handle_assignment(first_parsed.into_inner());
-            None
+            Err(Box::new(std::fmt::Error))
         } else {
-            Some(self.parse_expr(first_parsed.into_inner()))
+            self.eval_expr(first_parsed.into_inner())
         }
     }
 }
